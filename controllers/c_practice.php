31137b71e02ea4785c24106ab9c16b1a1a3d6508
Patrick Sinco <psinco@gmail.com>
2013-10-20 10:38:00 -0500

    minuscule change; just seeing effect of ths commit

--- controllers/c_practice.php
+++ controllers/c_practice.php
@@ -4,25 +4,30 @@
 
 class practice_controller extends base_controller
 {
-  public function __construct()
-  {
-    parent::__construct();
-    echo 'practice_controller just instantiated';
-  }
-
-  public function test1()
-  {
-    require(APP_PATH . '/libraries/Image.php');
-    echo 'you are looking at test1';
-    $imageObj = new Image('http://placekitten.com/1000/1000');
-    $imageObj->resize(200, 200);
-    $imageObj->display();
-  }
+    public function __construct()
+    {
+        parent::__construct();
+        echo 'practice_controller just instantiated';
+    }
   
-  public function test2()
-  {
-    echo Time::now();
-  }
+    public function test_db() 
+    {
+        
+    }
+
+    public function test1()
+    {
+        require(APP_PATH . '/libraries/Image.php');
+        echo 'you are looking at test1';
+        $imageObj = new Image('http://placekitten.com/1000/1000');
+        $imageObj->resize(200, 200);
+        $imageObj->display();
+    }
+    
+    public function test2()
+    {
+        echo Time::now();
+    }
 
 
 }
