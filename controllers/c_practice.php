1e448e277755efe11969c449ca25d3121c54675c
Patrick Sinco <psinco@gmail.com>
2013-10-20 17:34:53 -0500

    reflects changes thru lec-7-part-1

--- controllers/c_practice.php
+++ controllers/c_practice.php
@@ -7,12 +7,38 @@ class practice_controller extends base_controller
     public function __construct()
     {
         parent::__construct();
-        echo 'practice_controller just instantiated';
     }
   
     public function test_db() 
     {
+//        $q = "
+//            UPDATE users
+//            SET email = 'albert@aol.com'
+//            WHERE user_id = 3
+//        ";
+    
+//        $new_user = Array(
+//            'first_name' => 'Miguel',
+//            'last_name' => 'Cabrera',
+//            'email' => 'mcabrera@tigers.com'
+//        );
+        $q = "select email from users where first_name = 'Albert'";
         
+//        echo DB::instance(DB_NAME)->select_field($_POST['first_name']);
+            
+//        $q_clean= DB::instance(DB_NAME)->sanitize($q);
+        echo '<pre>';
+        print_r($_GET);
+//        echo '<br>';
+//        print_r($q_clean);
+        echo '</pre>';
+//        echo $q;
+//        DB::instance()->insert_row('users', $new_user);
+        
+//        echo '<pre>';
+//        print_r(DB::instance(DB_NAME)->query_history());
+//        echo '</pre>';
+                        
     }
 
     public function test1()
