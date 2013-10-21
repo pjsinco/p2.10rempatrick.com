17f9a696f963ed3cd3ab063b6489d1333150a0c6
Patrick Sinco <psinco@gmail.com>
2013-10-20 21:56:12 -0500

    reflects class notes thru lec 7

---
+++ views/v_users_login.php
@@ -0,0 +1,6 @@
+<h2>This is the login page</h2>
+<form action='/users/p_login' method='post'>
+    Email <input type='text' name='email'><br>
+    Password <input type='password' name='password'><br>
+    <input type='submit' value='Log in'>
+</form>
---
+++ views/v_users_signup.php
@@ -0,0 +1,8 @@
+<h2>This is the signup page</h2>
+<form action='/users/p_signup' method='post'>
+    First name <input type='text' name='first_name'><br>
+    Last name <input type='text' name='last_name'><br>
+    Email <input type='text' name='email'><br>
+    Password <input type='password' name='password'><br>
+    <input type='submit' value='Sign up'>
+</form>
