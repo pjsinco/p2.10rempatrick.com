76e4dd5877efabd8328f09b818e352ee7150c1eb
Patrick Sinco <psinco@gmail.com>
2013-11-06 16:41:24 -0600

    makes minor style changes

--- views/v_posts_add.php
+++ views/v_posts_add.php
@@ -2,8 +2,11 @@
       <div class='error'>Database error. Please try again</div>
     <?php endif; ?>
     <form action='/posts/p_add' method='POST' accept-charset='utf-8'>
-      <label for='content'><strong>New Post</strong></label><br>
-      <textarea class='post' name='content'></textarea>
-      <br><br>
-      <input type='submit' value='post'>
+      <fieldset>
+        <legend>New post</legend>
+        <p>
+          <textarea class='post' name='content'></textarea>
+        </p>
+        <input type='submit' class='submit' value='Post'>
+      <fieldset>
     </form>
